0f95d1052e5d0fe047454f0f6b65a3b68aa27564
RoboHoboMobo <georgivasilyev@yandex.ru>
2019-12-23 22:39:52 +0300

    rarm_keyboard_node was added

---
+++ rarm_control/include/rarm_keyboard_control.h
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include "std_msgs/Float64.h"
+#include "std_msgs/Int32.h"
+
+#include <string>
+#include <iostream>
+#include <curses.h>
+
+enum commands
+{
+  HOLD,
+  FORWARD,
+  BACKWARD,
+  RIGHT,
+  LEFT,
+  UP,
+  DOWN,
+  SPINUP,
+  SPINDOWN
+};
+
